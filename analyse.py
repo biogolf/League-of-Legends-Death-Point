3fa44078597b642cb966de2ee501671073da9515
biogolf <61070164@kmitl.ac.th>
2018-11-27 23:20:39 +0700

    Update analyse.py

--- analyse.py
+++ analyse.py
@@ -1,20 +1,23 @@
-def main()
+def main():
     import pandas as pd #this is how I usually import pandas
     import operator
-    url = r'https://github.com/biogolf/League-of-Legends-Death-Point/raw/master/kills.csv'
+    url = 'https://github.com/biogolf/League-of-Legends-Death-Point/raw/master/kills.csv'
     df = pd.read_csv(url)
     x = list(df['x_pos'])
-    x_pos = []
+    y = list(df['y_pos'])
+    xy_pos = []
     killer = {}
-    for i in x:
-        if not isinstance(i, str):
-            i = str(i)
-        if i.isdigit():
-            x_pos.append(i)
-    print(x_pos[0:5])
+    for i in range(len(x)):
+        if not isinstance(x[i], str):
+            x[i] = str(x[i])
+        if not isinstance(y[i], str):
+            y[i] = str(y[i])
+        if x[i].isdigit() and y[i].isdigit():
+            xy_pos.append((x[i], y[i]))
+    print(xy_pos[0:5])
 
     for j in list(df['Killer']):
-        if j not in z:
+        if j not in killer:
             killer[j] = 1
         else:
             killer[j] += 1
