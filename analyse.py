83f687ba5b724d98c4b9a4d93e2968bd57d3db1c
biogolf <61070164@kmitl.ac.th>
2018-11-29 14:38:32 +0700

    Update analyse.py

--- analyse.py
+++ analyse.py
@@ -1,11 +1,16 @@
+import pandas as pd
+import operator
+import matplotlib.pyplot as plt
 def main():
-    import pandas as pd
-    import operator
     url = 'https://github.com/biogolf/League-of-Legends-Death-Point/raw/master/kills.csv'
+    #url = r'C:\Users\USER\Documents\GitHub\League-of-Legends-Death-Point\kills.csv'
+    url2 = r'C:\Users\USER\Documents\GitHub\League-of-Legends-Death-Point\matchinfo.csv'
     df = pd.read_csv(url)
     x = list(df['x_pos'])
     y = list(df['y_pos'])
     xy_pos = []
+    x_pos = []
+    y_pos = []
     killer = {}
 
     #Saving the (x,y) in the xy_pos
@@ -15,7 +20,9 @@ def main():
         if not isinstance(y[i], str):
             y[i] = str(y[i])
         if x[i].isdigit() and y[i].isdigit():
-            xy_pos.append((x[i], y[i]))
+            xy_pos.append((int(x[i]), int(y[i])))
+            x_pos.append(int(x[i]))
+            y_pos.append(int(y[i]))
     print(xy_pos[0:5]) #Test print
 
     #Counting kills of the players and saving in dict killer
@@ -27,4 +34,9 @@ def main():
 
     sorted_killer = sorted(killer.items(), key=operator.itemgetter(1), reverse=True)
     print(sorted_killer[0:5]) #Test print
+
+    plt.scatter(x_pos, y_pos, label="test", color="k", s=0.002)
+    plt.title("League-of-Legends-Death-Point")
+
+    plt.show()
 main()
